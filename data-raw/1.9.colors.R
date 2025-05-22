d255eaf026391d8770bbeaaf790a441cb7d4613a
MOUNIER FLORENCE <florence.mounier@inrae.fr>
2025-05-22 20:23:09 +0200

    feat: use data for color palet

---
+++ data-raw/1.9.colors.R
@@ -0,0 +1,21 @@
+my_colors <- c(
+  "#1b9e77", # teal
+  "#d95f02", # orange
+  "#7570b3", # purple
+  "#e7298a", # pink
+  "#66a61e", # olive green
+  "#e6ab02", # mustard
+  "#a6761d", # brown
+  "#666666", # gray
+  "#1f78b4", # blue
+  "#b2df8a", # light green
+  "#fb9a99", # salmon
+  "#fdbf6f", # light orange
+  "#cab2d6", # lavender
+  "#ffff99", # light yellow
+  "#b15928", # dark brown
+  "#8dd3c7", # pale turquoise
+  "#ff69b4"  # hot pink
+)
+
+usethis::use_data(my_colors, overwrite = TRUE)
